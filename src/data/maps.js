2c06b6a986a2aafd480d8206f5a63af647991304
Mirko <mirko.friedrich@blandland.de>
2021-03-09 03:01:01 +0100

    init

---
+++ src/data/maps.js
@@ -0,0 +1,22 @@
+
+
+const maps = [
+  "Bobr",
+  "Brest, west",
+  "Haroshaje",
+  "Krupa",
+  "Orsha, east",
+  "Orsha, north",
+  "Shchedrin",
+  "Sianno",
+  "Siedlce",
+  "Slutsk",
+  "Slutsk, east",
+  "Slutsk, west",
+  "Thali Ihantala",
+  "Tsel",
+  "Zbuczyn",
+]
+
+
+export default maps
