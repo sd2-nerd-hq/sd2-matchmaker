0ddf5c85954412130b9fc0cfb52b4e7fb9fd7249
Mirko <mirko.friedrich@blandland.de>
2021-03-24 01:34:31 +0100

    add express

--- server/WebsocketServer.js
+++ server/WebsocketServer.js
@@ -1,12 +1,14 @@
-const fs = require( 'fs' );
-const http = require( 'https' );
 const WebSocket = require( 'ws' );
+const express = require("express")
+const INDEX = '/index.html';
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const server = http.createServer()
+
 const wss = new WebSocket.Server( { server } );
 
 const PORT = process.env.PORT || 8080
-server.listen(PORT)
 let subscribers = []
 
 
